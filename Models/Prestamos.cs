24eed27c7a2b8d19790f50015e067ea9de60ff59
Jerony Ulises Cruz Fermin <jerony_cruz@ucne.edu.do>
2024-09-30 21:19:45 -0400

    Se han agregado los RegularExpressions correctos

--- Models/Prestamos.cs
+++ Models/Prestamos.cs
@@ -8,12 +8,14 @@ namespace JeronyCruz_Ap1_P1.Models
         public int PrestamoId { get; set; }
 
         [Required(ErrorMessage ="Por favor ingresar el nombre del deudor")]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo se permiten letras")]
         public string Deudor { get; set; }
 
         [Required(ErrorMessage = "Por favor ingresar el concepto")]
         public string Concepto { get; set; }
 
         [Required(ErrorMessage = "Por favor ingresar el monto")]
+        [RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "Solo se permiten números enteros o decimales")]
         public double Monto { get; set; }
     }
 }
