bc070a8a9da69ab6efb41070a55f83d8b47afaba
Jerony Ulises Cruz Fermin <jerony_cruz@ucne.edu.do>
2024-10-07 23:19:20 -0400

    Creacion de Deudores

---
+++ Models/Deudores.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JeronyCruz_Ap1_P1.Models;
+
+public class Deudores
+{
+    [Key]
+    public int DeudorId { get; set; }
+
+    [Required(ErrorMessage = "Por favor ingresar el nombre del deudor")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo se permiten letras")]
+    public string Nombres { get; set;}
+}
