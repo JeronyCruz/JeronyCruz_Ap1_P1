a9e32312ef5404859629f0807693d4f7717c1cf0
Jerony Ulises Cruz Fermin <jerony_cruz@ucne.edu.do>
2024-10-07 23:20:27 -0400

    Modificaciones en el Context

--- DAL/Context.cs
+++ DAL/Context.cs
@@ -6,6 +6,17 @@ namespace JeronyCruz_Ap1_P1.DAL
     public class Context(DbContextOptions<Context> options) : DbContext(options)
     {
         public DbSet<Prestamos> Prestamos { get; set; }
+        public DbSet<Deudores> Deudores { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Deudores> ().HasData(new List<Deudores>()
+            {
+                new Deudores(){DeudorId = 1 , Nombres = "Juan Perez" },
+                new Deudores(){DeudorId = 2 , Nombres = "Celainy Lopez" },
+                new Deudores(){DeudorId = 3 , Nombres = "Wilmer Castillo" }
+            });
+        }
     }
 }
