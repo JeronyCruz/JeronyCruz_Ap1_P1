5a95c00d3f5759a44c2a821034668c2fe5ef5cab
Jerony Ulises Cruz Fermin <jerony_cruz@ucne.edu.do>
2024-10-12 18:30:27 -0400

    Creacion del Service CobroDetalles

---
+++ Services/CobrosDetallesServices.cs
@@ -0,0 +1,18 @@
+﻿using JeronyCruz_Ap1_P1.DAL;
+using JeronyCruz_Ap1_P1.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace JeronyCruz_Ap1_P1.Services
+{
+    public class CobrosDetallesServices(Context context)
+    {
+        private readonly Context _context = context;
+
+        public async Task<List<Prestamos>> Listar(Expression<Func<Prestamos, bool>> criterio)
+        {
+            return await _context.Prestamos
+                .AsNoTracking().Where(criterio).ToListAsync();
+        }
+    }
+}
