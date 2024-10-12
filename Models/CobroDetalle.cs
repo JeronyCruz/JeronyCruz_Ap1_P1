cdcd3767b9bbba2141f60b79415253ecaf115cd4
Jerony Ulises Cruz Fermin <jerony_cruz@ucne.edu.do>
2024-10-12 18:28:38 -0400

    Creacion de la clase CobroDetalle

---
+++ Models/CobroDetalle.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JeronyCruz_Ap1_P1.Models;
+
+    public class CobroDetalle
+    {
+        [Key]
+        public int DetalleId { get; set; }
+
+        public int CobroId  { get; set; }
+
+        [ForeignKey("Prestamos")]
+        public int PrestamoId { get; set; }
+        public Prestamos? Prestamos { get; set; }
+        
+
+        [Required(ErrorMessage ="Debe de ingresar el valor cobrado")]
+        [RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "Solo se permiten números enteros o decimales")]
+        public Double ValorCobrado { get; set; }
+    }
